5ea91e81f466743142087747c423d011f30f906a
joshiPi <bcasprabhatjoshi@gmail.com>
2021-09-06 13:00:56 +0530

    add: update

--- src/App.js
+++ src/App.js
@@ -7,8 +7,8 @@ import { CrossStorageHub } from 'cross-storage/lib/index'
 function App() {
   useEffect(() => {
     CrossStorageHub.init([
-      {origin: /.*localhost:300\d$/, allow: ['get']}
-      // {origin: /[a-z A-Z 1-9]+\.builtwithframe\.com$/, allow: ['get', 'set', 'del']}
+      {origin: /.*localhost:300\d$/, allow: ['get']},
+      {origin: /[a-z A-Z 1-9]+\.xvercel\.ml$/, allow: ['get', 'set', 'del']}
     ]);
   })
   return (
